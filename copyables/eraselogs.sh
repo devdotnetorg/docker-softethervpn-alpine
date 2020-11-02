dd9b7d2a92961e7a06c094d3eb00edd013850e02
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2020-11-03 01:31:42 +0300

    Update

--- copyables/eraselogs.sh
+++ copyables/eraselogs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "[Start eraselogs.sh]"
 MINUTES=$(($1 * 60))
-find /usr/vpnserver/server_log/ -name "*.log" -type f -mmin -$MINUTES -exec rm -f {} \;
-find /usr/vpnserver/packet_log/ -name "*.log" -type f -mmin -$MINUTES -exec rm -f {} \;
-find /usr/vpnserver/security_log/ -name "*.log" -type f -mmin -$MINUTES -exec rm -f {} \;
+find /usr/vpnserver/server_log/ -name "*.log" -type f -mmin +$MINUTES -exec rm -f {} \;
+find /usr/vpnserver/packet_log/ -name "*.log" -type f -mmin +$MINUTES -exec rm -f {} \;
+find /usr/vpnserver/security_log/ -name "*.log" -type f -mmin +$MINUTES -exec rm -f {} \;
 #rem
 mkdir /$(date +%s)
